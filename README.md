e4f1f5da24d4455bae6c6a6814a92f500df97b2b
JaiIn <di0308@naver.com>
2025-09-10 22:17:23 +0900

    init: 초기설정, 패키지 설치

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Bridge3
+# thridweb을 활용한 코인/NFT 민팅 연습
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        초기 설정
+      </div>
+    </>
+  )
+}
+
+export default App
